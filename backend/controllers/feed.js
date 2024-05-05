68d817b24e496595cebf5186821bbf598ba25091
GideonBature <infoaboutgideon@gmail.com>
2024-05-05 03:34:27 +0100

    add readme and part of client side application

---
+++ backend/controllers/feed.js
@@ -0,0 +1,24 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: "First Post",
+        content: "This is the first post",
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = {
+    id: new Date().toISOString(),
+    title: title,
+    content: content,
+  };
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: post,
+  });
+};
