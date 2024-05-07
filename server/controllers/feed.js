31bc6542ac932826e3c8ceec890462bbc5ddcc9c
GideonBature <infoaboutgideon@gmail.com>
2024-05-07 22:12:28 +0100

    add various files

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -2,9 +2,16 @@ exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: "1",
         title: "First Post",
         content: "This is the first post",
+        imageUrl: "../images/H.E Peter Obi.jpeg",
+        creator: {
+          name: "H.E Peter Obi"
+        },
+        createdAt: new Date(),
       },
+
     ],
   });
 };
@@ -13,9 +20,11 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const post = {
-    id: new Date().toISOString(),
+    _id: new Date().toISOString(),
     title: title,
     content: content,
+    creator: { name: "H.E Peter Obi" },
+    createdAt: new Date(),
   };
   res.status(201).json({
     message: "Post created successfully!",
