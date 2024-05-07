56fff6340be4c0d76552a613e051344721cc7068
GideonBature <infoaboutgideon@gmail.com>
2024-05-07 22:44:32 +0100

    added mongoose and connect it to the mongodb database

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -16,4 +17,8 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose.connect("mongodb://localhost/messaging-blog").then(() => {
+    app.listen(8080);
+}).catch(err => {
+    console.log(err);
+});
